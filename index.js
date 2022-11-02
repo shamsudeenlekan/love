6d48fa0d676892b1d6a070478ff671835c450d31
shamsudeenlekan <106394838+shamsudeenlekan@users.noreply.github.com>
2022-11-02 21:36:00 +0100

    Add love calculator site files

---
+++ index.js
@@ -0,0 +1,139 @@
+
+   let name = 'shamzy';
+   console.log('name');
+
+   function getMilk(params) {
+alert("leaveHouse");
+alert("moveRight");
+alert("moveRight");
+alert("moveUp");
+alert("moveUp");
+alert("moveUp");
+alert("moveUp");
+alert("moveRight");
+alert("moveRight");
+alert("buyMilk")
+alert("moveLeft");
+alert("moveLeft");
+alert("moveDown");
+alert("moveDown");
+alert("moveDown");
+alert("moveDown");
+alert("moveLeft");
+alert("moveLeft");
+alert("enterHouse");
+}
+
+function getMilk() {
+console.log("leaveHouse");
+console.log("moveRight");
+console.log("moveRight");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveRight");
+console.log("moveRight");
+console.log("buyMilk")
+console.log("moveLeft");
+console.log("moveLeft");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveLeft");
+console.log("moveLeft");
+console.log("enterHouse");
+}
+
+getMilk();
+
+
+
+
+
+
+function lifeInWeeks(age) {
+   var yearsRemaining = 90 - age;
+   var days = yearsRemaining * 365;
+   var weeks = yearsRemaining * 12;
+   var months= yearsRemaining * 52;
+
+   console.log("You have" + days + "days," + weeks + "weeks,"  + months + "months left." );
+
+}
+
+lifeInWeeks(20);
+
+
+
+
+
+
+(function Let assume you are 30 and you are to live for 120 years calculate how many years, months, weeks, days, hours, minutes and seconds you have left. There are 12 months, 52 weeks, 365 days, 24 hours, 60 minutes and 60 seconds)
+
+
+
+
+
+
+function totalUsed(age) {
+   var totalUsed = 120 - age;
+   var years = 120 - 30;
+   var months = totalUsed * 12;
+   var weeks = totalUsed * 52;
+   var days = totalUsed * 365;
+   var hours = totalUsed * 24;
+   var minutes = totalUsed * 60;
+   var seconds = totalUsed * 60;
+
+
+
+   console.log("You have " + years + "years, " + months +  "months ,"  + weeks + "weeks "  + days + "days " + hours + "hours "  + minutes +  "minutes"  +  seconds + "seconds"  );
+
+}
+
+
+
+totalUsed (30);
+
+
+function getMilk(money) {
+console.log("leaveHouse");
+console.log("moveRight");
+console.log("moveRight");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveRight");
+console.log("moveRight");
+
+var numberOfBottles = Math.floor(money / 1.5);
+
+console.log ("buy " + numberOfBottles + " bottles of Milk");
+
+
+
+console.log("moveLeft");
+console.log("moveLeft");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveLeft");
+console.log("moveLeft");
+console.log("enterHouse");
+}
+
+getMilk(5);
+
+
+
+
+
+
+
+var name = "shamzy";
+var text = "my name is";
+document.write (text + name);
---
+++ javascript/java
@@ -0,0 +1,25 @@
+
+
+
+
+prompt("what is your name?");
+prompt("what is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+
+if (loveScore > 70) {
+    alert ("Your love score is " + loveScore + "% " + "you love each other like bony and clair 🥺🤗❤️");
+
+}
+
+if (loveScore  === 31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68, 69) {
+   alert ("Your love score is " + loveScore + "% " + "awwwwn love kid 😂");
+}
+
+
+if (loveScore < 30) {
+   alert ("Your love score is " + loveScore + "% " + "omo i no fit laugh abeg 😂");
+}
+alert("This is not real. Love is a beautiful thing ☺️😍 I love you 🤗❤️")
